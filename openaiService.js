a8e15663b33bdd8c03d789e4a87896b73ba0d4e0
Jason Nathan <gurugeek@geekist.co>
2023-10-25 11:22:07 +0800

    aider: Refactored axios call to use the OpenAI SDK for making API requests.
    # Aider chat conversation:
    
    USER: modify the axios call to use the openai sdk that is installed
    
    ASSISTANT: To use the OpenAI SDK instead of axios for making API calls, we need to modify the `openaiService.js` file.
    
    Here are the steps:
    
    1. Import the OpenAI SDK at the top of the file.
    2. Initialize the OpenAI SDK with the API key.
    3. Replace the axios call with the OpenAI SDK's `createCompletion` method.
    
    Here is the *edit block* for these changes:
    
    ```javascript
    openaiService.js
    <<<<<<< HEAD
    const axios = require('axios');
    const dotenv = require('dotenv');
    
    dotenv.config();
    
    const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
    const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
    
    function sendToOpenAI(text) {
      const data = {
        prompt: `Estimate the total remuneration package for this candidate given the following CV:\n${text}`,
        max_tokens: 60
      };
    
      const config = {
        headers: {
          'Authorization': `Bearer ${OPENAI_API_KEY}`,
          'Content-Type': 'application/json'
        }
      };
    
      return axios.post(OPENAI_API_URL, data, config);
    }
    
    module.exports = { sendToOpenAI };
    =======
    const openai = require('openai');
    const dotenv = require('dotenv');
    
    dotenv.config();
    
    const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
    openai.apiKey = OPENAI_API_KEY;
    
    function sendToOpenAI(text) {
      const data = {
        engine: 'davinci-codex',
        prompt: `Estimate the total remuneration package for this candidate given the following CV:\n${text}`,
        max_tokens: 60
      };
    
      return openai.Completion.create(data);
    }
    
    module.exports = { sendToOpenAI };
    >>>>>>> updated
    ```
    
    After making these changes, the OpenAI SDK will be used for making API calls instead of axios.

--- openaiService.js
+++ openaiService.js
@@ -1,25 +1,19 @@
-const axios = require('axios');
+const openai = require('openai');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+openai.apiKey = OPENAI_API_KEY;
 
 function sendToOpenAI(text) {
   const data = {
+    engine: 'davinci-codex',
     prompt: `Estimate the total remuneration package for this candidate given the following CV:\n${text}`,
     max_tokens: 60
   };
 
-  const config = {
-    headers: {
-      'Authorization': `Bearer ${OPENAI_API_KEY}`,
-      'Content-Type': 'application/json'
-    }
-  };
-
-  return axios.post(OPENAI_API_URL, data, config);
+  return openai.Completion.create(data);
 }
 
 module.exports = { sendToOpenAI };
