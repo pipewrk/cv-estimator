72d67aefd979617c983df3ad7cf57c54f0f2398f
Jason Nathan <gurugeek@geekist.co>
2023-10-25 11:08:05 +0800

    aider: Replace `prompt` package with `inquirer` package for a more modern command line interface.
    # Aider chat conversation:
    
    USER: let's replace prompt with a more modern package
    
    ASSISTANT: Sure, we can replace the `prompt` package with the `inquirer` package, which is a more modern and flexible command line interface for Node.js.
    
    To do this, we need to modify two files: `app.js` and `package.json`.
    
    In `app.js`, we will replace the `prompt` import with `inquirer` and modify the code to use `inquirer.prompt` instead of `prompt.get`.
    
    In `package.json`, we will replace the `prompt` dependency with `inquirer`.
    
    Let's start with `app.js`:
    
    ```javascript
    app.js
    <<<<<<< HEAD
    const prompt = require('prompt');
    =======
    const inquirer = require('inquirer');
    >>>>>>> updated
    
    <<<<<<< HEAD
    prompt.start();
    
    prompt.get(['filePath'], function (err, result) {
      if (err) {
        console.error(err);
        return;
      }
    
      fileReader.readFile(result.filePath)
        .then(text => openaiService.sendToOpenAI(text))
        .then(response => console.log('Estimated remuneration package:', response.data.choices[0].text))
        .catch(err => console.error(err));
    });
    =======
    inquirer.prompt([
      {
        type: 'input',
        name: 'filePath',
        message: 'Enter the file path:'
      }
    ]).then(result => {
      fileReader.readFile(result.filePath)
        .then(text => openaiService.sendToOpenAI(text))
        .then(response => console.log('Estimated remuneration package:', response.data.choices[0].text))
        .catch(err => console.error(err));
    });
    >>>>>>> updated
    ```
    
    Now, let's update `package.json`:
    
    ```json
    package.json
    <<<<<<< HEAD
    "dependencies": {
      "axios": "^0.24.0",
      "textract": "^2.4.0",
      "prompt": "^2.0.1"
    }
    =======
    "dependencies": {
      "axios": "^0.24.0",
      "textract": "^2.4.0",
      "inquirer": "^8.2.0"
    }
    >>>>>>> updated
    ```
    
    Please note that you will need to run `npm install` after these changes to install the `inquirer` package and remove the `prompt` package.

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const prompt = require('prompt');
+const inquirer = require('inquirer');
 const fileReader = require('./fileReader');
 const openaiService = require('./openaiService');
 
-prompt.start();
-
-prompt.get(['filePath'], function (err, result) {
-  if (err) {
-    console.error(err);
-    return;
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'filePath',
+    message: 'Enter the file path:'
   }
-
+]).then(result => {
   fileReader.readFile(result.filePath)
     .then(text => openaiService.sendToOpenAI(text))
     .then(response => console.log('Estimated remuneration package:', response.data.choices[0].text))
