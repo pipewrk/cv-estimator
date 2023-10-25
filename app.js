36b82fddb5234507aa043796a87cd56cd5fbc0c5
Jason Nathan <gurugeek@geekist.co>
2023-10-25 11:34:37 +0800

    aider: Change the references to `fileReader.readFile` and `openaiService.sendToOpenAI` to `readFile` and `sendToOpenAI` respectively in `app.js`.
    # Aider chat conversation:
    
    USER: please sure the imports are correctly r4eferences in usage
    
    ASSISTANT: I see that there are some inconsistencies in the way the imported modules are referenced in the `app.js` file. The `readFile` function is imported from `fileReader.js` and the `sendToOpenAI` function is imported from `openaiService.js`, but they are referenced as `fileReader.readFile` and `openaiService.sendToOpenAI` respectively.
    
    Here is the *edit block* for the necessary changes:
    
    ```javascript
    app.js
    <<<<<<< HEAD
    inquirer.prompt([
      {
        type: 'input',
        name: 'filePath',
        message: 'Enter the file path:'
      }
    ]).then(result => {
      fileReader.readFile(result.filePath)
        .then(text => openaiService.sendToOpenAI(text))
        .then(response => console.log('Estimated remuneration package:', response.data.choices[0].text))
        .catch(err => console.error(err));
    });
    =======
    inquirer.prompt([
      {
        type: 'input',
        name: 'filePath',
        message: 'Enter the file path:'
      }
    ]).then(result => {
      readFile(result.filePath)
        .then(text => sendToOpenAI(text))
        .then(response => console.log('Estimated remuneration package:', response.data.choices[0].text))
        .catch(err => console.error(err));
    });
    >>>>>>> updated
    ```
    
    Please make the suggested changes to the `app.js` file and let me know if you encounter any further issues.

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ inquirer.prompt([
     message: 'Enter the file path:'
   }
 ]).then(result => {
-  fileReader.readFile(result.filePath)
-    .then(text => openaiService.sendToOpenAI(text))
+  readFile(result.filePath)
+    .then(text => sendToOpenAI(text))
     .then(response => console.log('Estimated remuneration package:', response.data.choices[0].text))
     .catch(err => console.error(err));
 });
